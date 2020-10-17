2d73ebebbadd430cbec8323a3574fde2400d8af7
aparins <31795093+MrGexik85@users.noreply.github.com>
2020-10-17 15:47:30 +0300

    Add files via upload
    Поддержка только windows

--- load.c
+++ load.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-#ifdef WIN32
+//#ifdef WIN32
     #include <windows.h>
-#else
-    #include <dlfcn.h>
-#endif
+//#else
+  //  #include <dlfcn.h>
+//#endif
 
 #include "load.h"
 
@@ -13,11 +13,11 @@ int LoadRun(const char * const s, int i, int *mas, int size) {
     void * lib;
     int (*fun)(int *el, int size);
 
-    #ifdef WIN32
+    //#ifdef WIN32
         lib = LoadLibrary(s);
-    #else
-        lib = dlopen(s, RTLD_LAZY);
-    #endif
+    //#else
+      //  lib = dlopen(s, RTLD_LAZY);
+    //#endif
 
     if (!lib) {
         printf("cannot open library '%s'\n", s);
@@ -25,32 +25,32 @@ int LoadRun(const char * const s, int i, int *mas, int size) {
     }
 
     if (i==1){
-        #ifdef WIN32
+        //#ifdef WIN32
             fun = (void (*)(int *mas, int size))GetProcAddress((HINSTANCE)lib, "get_mas_count");
-        #else
-            fun = (void (*)(int *mas, int size))dlsym(lib, "get_mas_count");
-        #endif
+        //#else
+          //  fun = (void (*)(int *mas, int size))dlsym(lib, //"get_mas_count");
+        //#endif
 
         if (fun == NULL) {
             printf("cannot load function \n");
         }
     } else if (i==2){
-        #ifdef WIN32
+       // #ifdef WIN32
             fun = (void (*)(int *matr, int size))GetProcAddress((HINSTANCE)lib, "get_matr_count");
-        #else
-            fun = (void (*)(int *matr, int size))dlsym(lib, "get_matr_count");
-        #endif
+        //#else
+          //  fun = (void (*)(int *matr, int size))dlsym(lib, //"get_matr_count");
+   //     #endif
 
         if (fun == NULL) {
             printf("cannot load function \n");
         }
     }
     count_m = fun(mas, size);
-    #ifdef WIN32
+    //#ifdef WIN32
         FreeLibrary((HINSTANCE)lib);
-    #else
-        dlclose(lib);
-    #endif
+    //#else
+      //  dlclose(lib);
+    //#endif
 
     return count_m;
 
